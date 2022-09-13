53b00125919780d13dd0c6499d54bbff5b0a51da
maia <maia.perderizet@etudiant.univ-reims.fr>
2022-09-13 22:33:23 +0200

    Creation des methodes de la classe calculator

--- ScoreTarot/Model/Calculator.cs
+++ ScoreTarot/Model/Calculator.cs
@@ -6,7 +6,71 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    internal class Calculator
+    public class Calculator
     {
+        
+        public int calculScoreAutreJoueur(int scoreJoueurQuiPrend, int nbJoueur)
+        {
+                return scoreJoueurQuiPrend/(nbJoueur-1);
+        }
+
+        public int calculeScoreJoueurQuiPrend(List<Bonus> bonus, Contrat contrat, int scoreJoueur, bool gagne, int nbJoueur)
+        {
+            int score = 0;
+            bool petit = false;
+            foreach (Bonus b in bonus)
+            {
+                if(b == Bonus.PetitAuBout)
+                {
+                    petit = true;
+                } else if(b == Bonus.Poignet)
+                {
+                    score += 10;
+                }
+            }
+            if(contrat == Contrat.PRISE) {
+                score += scoreJoueur;
+                if (petit)
+                {
+                    score += 10;
+                }
+            } else if (contrat == Contrat.GARDE) {
+                score += scoreJoueur*2;
+                if (petit)
+                {
+                    score += 20;
+                }
+            } else if (contrat == Contrat.GARDE_SANS) {
+                score += scoreJoueur*4;
+                if (petit)
+                {
+                    score += 30;
+                }
+            } else if (contrat == Contrat.GARDE_CONTRE) {
+                score += scoreJoueur*6;
+                if (petit)
+                {
+                    score += 40;
+                }
+            }
+            if (!gagne) { score = -score; }
+            return score*(nbJoueur-1);
+
+        }
+
+        public float calculerPourcentageDeReussite(int nbPartieRealise, int nbPartieGagne)
+        {
+            return (nbPartieGagne * 100) / nbPartieRealise;
+        }
+
+        public int scoreTotalDuJoueur(List<Partie> parties)
+        {
+            int scoreTotal = 0;
+            foreach(Partie partie in parties)
+            {
+                scoreTotal += partie.getScoreJoueur();
+            }
+            return scoreTotal;
+        }
     }
 }
