35208861d3cfc703ab2f49dffe5e7d93a126c3f7
cebonal1 <Cecile.BONAL@etu.uca.fr>
2022-09-16 23:41:18 +0200

    stub + stubmanager

---
+++ ScoreTarot/Stub/ManagerStub.cs
@@ -0,0 +1,24 @@
+﻿
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stub
+{
+    public class ManagerStub : ILoader
+    {
+        public Joueur loadJoueur()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Partie loadPartie()
+        {
+            //return new StubPartie().getDesPartie();
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ScoreTarot/Stub/StubPartie.cs
@@ -0,0 +1,19 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stub
+{
+    public class StubPartie 
+    {
+        public List<Partie> getDesPartie()
+        {
+            List<Partie> lespartie = new List<Partie>();
+            
+            return lespartie;
+        }
+    }
+}
