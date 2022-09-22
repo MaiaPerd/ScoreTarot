7e9b1af92fc711ec791b73fc5a030bbfa7a876b3
Maia PERDERIZET <maperderiz>
2022-09-22 12:03:20 +0200

    TestEqual manche

---
+++ ScoreTarot/TestsUnitaires/DataTest.cs
@@ -0,0 +1,33 @@
+﻿using Model;
+using Stub;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsUnitaires
+{
+    public class DataTest
+    {
+
+        public static IEnumerable<object[]> Data_TestEqualsManche()
+        {
+            StubBonus stubBonus = new StubBonus();
+            yield return new object[]
+            {
+                true,
+                new Manche(Contrat.GardeContre, new Joueur("Joueur", 0), 1, 50, stubBonus.chargerListeBonusMoyen()),
+                new Manche(Contrat.GardeContre, new Joueur("Joueur", 0), 1, 50, stubBonus.chargerListeBonusMoyen())
+            };
+            yield return new object[]
+            {
+                false,
+                new Manche(Contrat.GardeContre, new Joueur("Joueur", 0), 1, 50, stubBonus.chargerListeBonusMoyen()),
+                new Manche(Contrat.GardeContre, new Joueur("Joueur", 0), 2, 50, stubBonus.chargerListeBonusMoyen())
+            };
+        }
+
+
+    }
+}
--- ScoreTarot/TestsUnitaires/UnitTestManche.cs
+++ ScoreTarot/TestsUnitaires/UnitTestManche.cs
@@ -5,50 +5,25 @@ namespace TestsUnitaires;
 
 public class UnitTestManche
 {
-    private StubBonus stubBonus = new StubBonus();
-    private StubManche stubManche = new StubManche();
-    private StubPartie stubPartie = new StubPartie();
-    private StubJoueur stubJoueur = new StubJoueur();
+    
 
     [Theory]
-    [InlineData(Contrat.Garde, 1, 10)]
-    [InlineData(Contrat.GardeContre, 1, 10)]
-    [InlineData(Contrat.Prise, 1, 10)]
-    [InlineData(Contrat.GardeSans, 1, 10)]
-    [InlineData(Contrat.Garde, null, 10)]
-    [InlineData(Contrat.Garde, 1, null)]
-    [InlineData(null, 1, 10)]
-    [InlineData(Contrat.Garde, 0, -1.4)]
-    [InlineData(null, null, -1.4)]
-    [InlineData(null, null, null)]
-    [InlineData(Contrat.Garde, null, null)]
-    [InlineData(null, 0, null)]
-    [InlineData(Contrat.Garde, 0939393, 1)]
-    public void TestConstructeurMancheAvecId(Contrat contrat, int id, int score)
+    [MemberData(nameof(StubManche.chargerLesManche3J))]
+    public void TestConstructeurMancheAvecId(Contrat contrat, Joueur joueurQuiPrend, int id, int score, List<Bonus> bonus, Joueur joueurAllier)
     {
-        Joueur joueur = new Joueur("Joueur", 0);
-        Manche manche = new Manche(contrat, joueur, id, score, stubBonus.chargerListeBonusBien(), joueur);
+        Manche manche = new Manche( contrat, joueurQuiPrend, id, score, bonus, joueurAllier);
         Assert.Equal(contrat, manche.Contrat);
-        Assert.Equal(joueur, manche.JoueurQuiPrend);
-        Assert.Equal(joueur, manche.JoueurAllier);
+        Assert.Equal(joueurQuiPrend, manche.JoueurQuiPrend);
+        Assert.Equal(joueurAllier, manche.JoueurAllier);
         Assert.Equal(id, manche.Id);
-        Assert.Equal(stubBonus.chargerListeBonusBien(), manche.Bonus);
+        Assert.Equal(bonus, manche.Bonus);
     }
 
-    [Fact]
-    public void TestEqualsManche()
+    [Theory]
+    [MemberData(nameof(DataTest.Data_TestEqualsManche))]
+    public void TestEqualsManche(bool equal, Manche manche1, Manche manche2)
     {
-        Manche manche = stubManche.chargerUneManche(stubJoueur.chargerJoueurPartie3J());
-        Manche manche2 = stubManche.chargerUneManche(stubJoueur.chargerJoueurPartie3J());
-        Assert.Equal(true, manche.Equals(manche2));
-    }
-
-    [Fact]
-    public void TestNotEqualsManche()
-    {
-        Manche manche = stubManche.chargerUneManche(stubJoueur.chargerJoueurPartie3J());
-        Manche manche2 = stubManche.chargerUneManche2(stubJoueur.chargerJoueurPartie3J());
-        Assert.Equal(false, manche.Equals(manche2));
+        Assert.Equal(equal, manche1.Equals(manche2));
     }
 
     
