74cfd9792c22954928cfaed36258d31efbe1ebca
maia <maia.perderizet@etudiant.univ-reims.fr>
2022-10-23 12:19:55 +0200

    ajout DataManager app

---
+++ ScoreTarot/AppliConsole/Gestionnaire/DataManager.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using Model;
+using Model.Interface;
+using Stub;
+
+namespace AppliConsole.Gestionnaire
+{
+    public class DataManager 
+    {
+        object dataManager;
+
+        bool BD;
+
+        public DataManager()
+        {
+            getBDNull();
+            if (BD)
+            {
+                dataManager = new Stub.ManagerStub();
+                ((Stub.ManagerStub)dataManager).LoadJoueur().ForEach(j => addJoueur(j));
+                ((Stub.ManagerStub)dataManager).LoadPartie().ForEach(p => addPartie(p));
+            }
+            else
+            {
+                dataManager = new EntityFramework.DataManager();
+            }
+        }
+
+        public async Task getBDNull()
+        {
+            EntityFramework.DataManager d = new EntityFramework.DataManager();
+            IEnumerable<Joueur> j = await d.getJoueurs();
+            if (j.Count() == 0)
+            {
+                BD = true;
+            }
+            else
+            {
+                BD = false;
+            }
+        }
+        
+        public Task<bool> addJoueur(Joueur joueur)
+        {
+            return ((EntityFramework.DataManager)dataManager).addJoueur(joueur);
+        }
+
+        public Task<bool> addManche(Manche manche)
+        {
+            return ((EntityFramework.DataManager)dataManager).addManche(manche);   
+        }
+
+        public Task<bool> addPartie(Partie partie)
+        {
+            return ((EntityFramework.DataManager)dataManager).addPartie(partie);
+        }
+
+        public Task clearJoueurs()
+        {
+            return ((EntityFramework.DataManager)dataManager).clearJoueurs();  
+        }
+
+        public Task clearManches()
+        {
+            return ((EntityFramework.DataManager)dataManager).clearManches(); 
+        }
+
+        public Task clearParties()
+        {
+            return ((EntityFramework.DataManager)dataManager).clearParties(); 
+        }
+
+        public Task<Joueur> getJoueur(string pseudo)
+        {
+            return ((EntityFramework.DataManager)dataManager).getJoueur(pseudo);   
+        }
+
+        public Task<IEnumerable<Joueur>> getJoueurPartie(int id)
+        {
+            return ((EntityFramework.DataManager)dataManager).getJoueurPartie(id);           
+        }
+
+        public Task<IEnumerable<Joueur>> getJoueurs()
+        {
+            return ((EntityFramework.DataManager)dataManager).getJoueurs();
+        }
+
+        public Task<Manche> getManche(int id)
+        {
+            return ((EntityFramework.DataManager)dataManager).getManche(id);
+        }
+
+        public Task<IEnumerable<Manche>> getManchePartie(int id)
+        {
+            return ((EntityFramework.DataManager)dataManager).getManchePartie(id);
+        }
+
+        public Task<IEnumerable<Manche>> getManches()
+        {
+            return ((EntityFramework.DataManager)dataManager).getManches();
+        }
+
+        public Task<Partie> getPartie(int id)
+        {
+            return ((EntityFramework.DataManager)dataManager).getPartie(id);
+        }
+
+        public Task<IEnumerable<Partie>> getPartieJoueur(string pseudo)
+        {
+            return ((EntityFramework.DataManager)dataManager).getPartieJoueur(pseudo);
+        }
+
+        public Task<IEnumerable<Partie>> getParties()
+        {
+            return ((EntityFramework.DataManager)dataManager).getParties();
+        }
+
+        public Task<IEnumerable<Partie>> loadPartie(IEnumerable<Joueur> listJoueur)
+        {
+            return ((EntityFramework.DataManager)dataManager).loadPartie(listJoueur);
+        }
+
+        public Task<bool> removeJoueur(Joueur joueur)
+        {
+            return ((EntityFramework.DataManager)dataManager).removeJoueur(joueur);
+        }
+
+        public Task<bool> removeManche(Manche manche)
+        {
+            return ((EntityFramework.DataManager)dataManager).removeManche(manche);
+        }
+
+        public Task<bool> removePartie(Partie partie)
+        {
+            return ((EntityFramework.DataManager)dataManager).removePartie(partie);
+        }
+
+        public Task<bool> removePartieDuJoueur(Joueur joueur)
+        {
+            return ((EntityFramework.DataManager)dataManager).removePartieDuJoueur(joueur);
+        }
+
+        public Task<bool> updateJoueur(Joueur joueur)
+        {
+            return ((EntityFramework.DataManager)dataManager).updateJoueur(joueur);
+        }
+
+        public Task<bool> updateManche(Manche manche)
+        {
+            return ((EntityFramework.DataManager)dataManager).updateManche(manche);
+        }
+
+        public Task<bool> updatePartie(Partie partie)
+        {
+            return ((EntityFramework.DataManager)dataManager).updatePartie(partie);
+        }
+    }
+}
+
--- ScoreTarot/EntityFramework/Gestionnaire/DataManager.cs
+++ ScoreTarot/EntityFramework/Gestionnaire/DataManager.cs
@@ -1,5 +1,6 @@
 ﻿using EntityFramework.Entity;
 using Model;
+using Model.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace EntityFramework
 {
-    public class DataManager : IDataManager
+    public class DataManager : ILoader, ISaver
     {
 
         public async Task<bool> addJoueur(Joueur joueur)
@@ -83,6 +84,20 @@ namespace EntityFramework
             return joueur;
         }
 
+        public async Task<IEnumerable<Joueur>> getJoueurPartie(int id)
+        {
+            List<Joueur> joueurs = new();
+            using (var context = new SQLiteContext())
+            {
+                IEnumerable<PartieJoueur> partieJoueurs = context.PartieJoueurs.Where(joueur => joueur.PartieForeignKey == id);
+          
+                await Task.Run(() =>
+                    joueurs.AddRange(partieJoueurs.Select(partieJ => context.Joueurs.Find(partieJ.JoueurForeignKey).toModel())));
+            }
+            return joueurs;
+            
+        }
+
         public async Task<IEnumerable<Joueur>> getJoueurs()
         {
             List<Joueur> joueurs = new();
@@ -100,6 +115,18 @@ namespace EntityFramework
             using (var context = new SQLiteContext())
             {
                 manche = context.Manches.Where(m => m.Id.Equals(id)).First().toModel();
+            }
+            return manche;
+        }
+
+        public async Task<IEnumerable<Manche>> getManchePartie(int id)
+        {
+            List<Manche> manche = new();
+            using (var context = new SQLiteContext())
+            {
+                Partie partie = await getPartie(id);
+                await Task.Run(() =>
+                    manche.AddRange(partie.Manches));
             }
             return manche;
         }
@@ -125,6 +152,19 @@ namespace EntityFramework
             return partie;
         }
 
+        public async Task<IEnumerable<Partie>> getPartieJoueur(string pseudo)
+        {
+            List<Partie> parties = new();
+            using (var context = new SQLiteContext())
+            {
+                IEnumerable<PartieJoueur> partieJoueurs = context.PartieJoueurs.Where(joueur => joueur.JoueurForeignKey == pseudo);
+
+                await Task.Run(() =>
+                    parties.AddRange(partieJoueurs.Select(partieJ => context.Parties.Find(partieJ.PartieForeignKey).toModel())));
+            }
+            return parties;
+        }
+
         public async Task<IEnumerable<Partie>> getParties()
         {
             List<Partie> parties = new();
@@ -136,12 +176,24 @@ namespace EntityFramework
             return parties;
         }
 
+        public async Task<IEnumerable<Partie>> loadPartie(IEnumerable<Joueur> listJoueur)
+        {
+            List<Partie> parties = new();
+            using (var context = new SQLiteContext())
+            {
+                await Task.Run(() =>
+                   parties.AddRange(context.Parties.Select(p => p.toModel())));
+            }
+            return parties;
+        }
+
         public async Task<bool> removeJoueur(Joueur joueur)
         {
             bool result = false;
             using (var context = new SQLiteContext())
             {
                 context.Remove(joueur.toEntity());
+                context.Remove(context.Parties.Where(m => m.Joueurs.Contains(joueur.toEntity())));
                 result = await context.SaveChangesAsync() == 1;
             }
             return result;
@@ -164,6 +216,24 @@ namespace EntityFramework
             using (var context = new SQLiteContext())
             {
                 context.Remove(partie.toEntity());
+                result = await context.SaveChangesAsync() == 1;
+            }
+            return result;
+        }
+
+        public async Task<bool> removePartieDuJoueur(Joueur joueur)
+        {
+            bool result = false;
+            using (var context = new SQLiteContext())
+            {
+                List<Partie> p = new();
+                p.AddRange(context.Parties.Select(partie => partie.toModel()));
+                p.ForEach(partie => {
+                    if (partie.Joueurs.Contains(joueur))
+                    {
+                        context.Remove(partie.toEntity());
+                    }
+                    });
                 result = await context.SaveChangesAsync() == 1;
             }
             return result;
