0e6146347a8c6f22caf9801f99ea8aa2bfe1d0cc
Maia PERDERIZET <maperderiz>
2023-01-10 17:10:44 +0100

    ajout des DTOs

--- ScoreTarot/DTOs/JoueurDto.cs
+++ ScoreTarot/DTOs/JoueurDto.cs
@@ -3,15 +3,10 @@
     public class JoueurDto
     {
         public int Id { get; set; }
-
         public string Pseudo { get; set; }
-
         public string Nom { get; set; }
-
         public string Prenom { get; set; }
-
         public int Age { get; set; }
-
         public string URLIMG { get; private set; }
     }
 }
---
+++ ScoreTarot/DTOs/PartieDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs
+{
+    public class PartieDto
+    {
+        public  List<JoueurDto> Joueurs { get; set; }
+        public List<MancheDto> Manches { get; set; }
+        public int Id { get; set; }
+    }
+}
