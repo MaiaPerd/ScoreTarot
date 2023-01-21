52da5b2abcc2e5eb7f32578c028786a98ccfca30
Maia PERDERIZET <maperderiz>
2023-01-21 11:09:28 +0100

    mise en place de graphQl

---
+++ ScoreTarot/APIGraphQL/Query/Query.cs
@@ -0,0 +1,21 @@
+﻿using DTOs;
+
+namespace APIGraphQL.Query
+{
+    public class Query
+    {
+        public JoueurDto GetJoueur() =>
+            new JoueurDto
+            {
+                Id = 0,
+                Pseudo = "test"
+            };
+
+        public MancheDto GetManche() =>
+            new MancheDto
+            {
+                Id = 0,
+                JoueurQuiPrend = new JoueurDto { Id = 1 },
+            };
+    }
+}
