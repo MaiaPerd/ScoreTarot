1666ccd3b260c30e146034cbf6530523968e252a
Maia PERDERIZET <maperderiz>
2023-01-21 11:58:42 +0100

    Ajout de la classe mutation

---
+++ ScoreTarot/APIGraphQL/Query/Mutation.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using DTOs;
+using EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using System.Net;
+
+namespace APIGraphQL.Query
+{
+    public class Mutation
+    {
+        private readonly DataManager dataManager;
+        private readonly IMapper mapper;
+
+        public Mutation(DataManager dataManager, IMapper mapper)
+        {
+            this.dataManager = dataManager;
+            this.mapper = mapper;
+        }
+
+        public async Task<bool> AddJoueur(JoueurDto joueur)
+        {
+            // return joueur
+            return await dataManager.AddJoueur(mapper.Map<Joueur>(joueur));
+        }
+
+        public async Task<bool> UpdateJoueur(JoueurDto joueur)
+        {
+            // update l'id et la modification
+            //https://chillicream.com/docs/hotchocolate/v12/defining-a-schema/mutations
+            return await dataManager.UpdateJoueur(mapper.Map<Joueur>(joueur));
+        }
+
+        public async Task<bool> DeleteJoueur(JoueurDto joueur)
+        {
+            return await dataManager.RemoveJoueur(mapper.Map<Joueur>(joueur));
+        }
+    }
+
+}
